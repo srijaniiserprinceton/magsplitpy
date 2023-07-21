0505869fea276fecebfd560958748f14d9e8f44c
srijaniiserprinceton <sbdas@princeton.edu>
2023-07-21 16:27:06 +0200

    Minor change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# hopefully can remove this set of imports and sys.append after making magsplitpy a package
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
